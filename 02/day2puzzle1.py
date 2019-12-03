f141aa84283145e31ff1e43a9066a13965ff1a0d
Olli Väisänen <olli.vaisanen@iki.fi>
2019-12-03 13:02:46 +0200

    Day 2 puzzles start
    Beginning of solution for day 2 puzzle 1

---
+++ 02/day2puzzle1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 17:32:27 2019
+
+@author: Olli
+"""
+
+test_prog = [1,0,0,0,99]
+
+pos = 0
+while pos >= 0:
+    if test_prog[pos] == 99:
+        break
+    else if test_prog[pos] == 1:
+        result = 
